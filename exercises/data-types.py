1a37c6ca2e657cc3348ece3008a6885de34a2acf
apangr <adrianpangrojas94@gmail.com>
2025-07-23 13:58:13 +0200

    feat: data types exercise

---
+++ exercises/data-types.py
@@ -0,0 +1,11 @@
+# Variables con diferentes tipos de datos
+age = 25  # Número entero
+height = 1.75  # Número flotante
+is_student = True  # Valor booleano
+name = "Juan"  # Cadena de texto
+
+# Imprimir cada variable en una línea separada
+print(age)
+print(height)
+print(is_student)
+print(name)
